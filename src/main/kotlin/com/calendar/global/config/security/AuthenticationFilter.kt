d01b606af18122b98d07182d661122b74b50561f
HKL <bastlwalzer@gmail.com>
2023-01-01 19:58:12 +0900

    Java To Kotlin 오류 잡아보장!

---
+++ src/main/kotlin/com/calendar/global/config/security/AuthenticationFilter.kt
@@ -0,0 +1,31 @@
+package com.calendar.global.config.security
+
+import jakarta.servlet.FilterChain
+import jakarta.servlet.ServletException
+import jakarta.servlet.ServletRequest
+import jakarta.servlet.ServletResponse
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.web.filter.GenericFilterBean
+import java.io.IOException
+
+@RequiredArgsConstructor
+class AuthenticationFilter : GenericFilterBean() {
+
+    private val tokenProvider: TokenProvider? = null
+
+    @Throws(IOException::class, ServletException::class)
+    override fun doFilter(request: ServletRequest?, response: ServletResponse?, chain: FilterChain) {
+        // 헤더에서 JWT 를 받아옵니다.
+        val token: String = tokenProvider.resolveToken(request as HttpServletRequest?)
+        // 유효한 토큰인지 확인합니다.
+        if (token != null && tokenProvider.validateToken(token)) {
+            // 토큰이 유효하면 토큰으로부터 유저 정보를 받아옵니다.
+            val authentication: Authentication = tokenProvider.getAuthentication(token)
+            // SecurityContext 에 Authentication 객체를 저장합니다.
+            SecurityContextHolder.getContext().authentication = authentication
+        }
+        chain.doFilter(request, response)
+    }
+}
---
+++ src/main/kotlin/com/calendar/global/config/security/TokenProvider.kt
@@ -0,0 +1,72 @@
+package com.calendar.global.config.security
+
+import jakarta.annotation.PostConstruct
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.stereotype.Component
+import java.util.*
+
+@RequiredArgsConstructor
+@Component
+@Log4j2
+class TokenProvider {
+    private var secretKey = "hangry"
+
+    //토큰 유효시간 30분
+    private val tokenValidTime = 30 * 60 * 1000L
+    private val redisTemplate: StringRedisTemplate? = null
+    private val userDetailsService: UserDetailsService? = null
+    @PostConstruct
+    protected fun init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.toByteArray())
+    }
+
+    //JWT 토큰 생성
+    fun createToken(member: MemberEntity): String {
+        val claims: Claims = Jwts.claims().setSubject(member.getUsername()) //JWT payload에 저장되는 단위
+        claims.put("mberSe", member.getMberSe()) // 정보는 key / value 쌍으로 저장된다.
+        val now = Date()
+        val token: String = Jwts.builder()
+                .setClaims(claims) // 정보 저장
+                .setIssuedAt(now) // 토큰 발행 시간 정보
+                .setExpiration(Date(now.time + tokenValidTime)) // set Expire Time
+                .signWith(SignatureAlgorithm.HS256, secretKey) // 사용할 암호화 알고리즘과 signature 에 들어갈 secret값 세팅
+                .compact()
+        val valueOperations: ValueOperations<String, String> = redisTemplate.opsForValue()
+        valueOperations.set(token, member.toString()) // redis set 명령어
+        return token
+    }
+
+    //JWT 토큰에서 인증 정보 조회
+    fun getAuthentication(token: String?): Authentication {
+        val userDetails = userDetailsService!!.loadUserByUsername(getUserToken(token))
+        return UsernamePasswordAuthenticationToken(userDetails, "", userDetails.authorities)
+    }
+
+    // 토큰에서 회원 정보 추출
+    fun getUserToken(token: String?): String {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject()
+    }
+
+    // Request의 Header에서 token 값을 가져옵니다. "X-AUTH-TOKEN" : "TOKEN값'
+    fun resolveToken(request: HttpServletRequest): String {
+        return request.getHeader("X-AUTH-TOKEN")
+    }
+
+    // 토큰의 유효성 + 만료일자 확인
+    fun validateToken(jwtToken: String?): Boolean {
+        return try {
+            val claims: Jws<Claims> = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken)
+            val valueOperations: ValueOperations<String, String> = redisTemplate.opsForValue()
+            if (ObjectUtil.isEqualStr(CommonConstants.CONST_LOGOUT, valueOperations.get(jwtToken))) {
+                log.info("로그아웃된 토큰 입니다.")
+                return false
+            }
+            !claims.getBody().getExpiration().before(Date())
+        } catch (e: Exception) {
+            false
+        }
+    }
+}
