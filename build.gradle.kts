5df0896d64d2ee0238bf21b5fc211c35792a7114
HKL <bastlwalzer@gmail.com>
2023-01-14 21:47:28 +0900

    QueryDSL 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
-    implementation("org.postgresql:postgresql")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
@@ -35,6 +34,10 @@ dependencies {
     implementation("commons-lang:commons-lang:2.6")
     implementation("com.fasterxml.uuid:java-uuid-generator:4.0.1")
     implementation("org.modelmapper:modelmapper:3.1.0")
+
+    //DB
+    implementation("org.postgresql:postgresql")
+    implementation("org.mariadb.jdbc:mariadb-java-client")
 
     //JWT 0.11.5 버전..
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
@@ -46,8 +49,14 @@ dependencies {
     // QueryDSL 스프링 부트 3.0 이상
     implementation("com.querydsl:querydsl-jpa:$querydslVersion:jakarta")
     kapt("com.querydsl:querydsl-apt:$querydslVersion:jakarta")
+    kapt("org.springframework.boot:spring-boot-configuration-processor")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+// QueryDSL이 만들어주는 Qclass를 사용하기 위해 저 위치로 접근할 수 있도록 설정해주는 부분이다.
+sourceSets["main"].withConvention(org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet::class) {
+    kotlin.srcDir("$buildDir/generated/source/kapt/main")
 }
 
 tasks.withType<KotlinCompile> {
